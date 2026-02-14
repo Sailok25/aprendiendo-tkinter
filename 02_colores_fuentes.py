737c676e0275585a910ae711bda6c0d64fddda13
Alba Ayala <albaayvi4@gmail.com>
2026-02-14 13:20:12 +0100

    Ej2: "Colores y fuentes" completado

--- 02_colores_fuentes.py
+++ 02_colores_fuentes.py
@@ -8,7 +8,13 @@ ventana.geometry("600x400")
 lbl_con_color_fondo = ttk.Label(ventana, text='Un texto con color de fondo!', background='green', foreground='white')
 lbl_con_color_fondo.pack()
 
-lbl_con_fuente = tk.Label(ventana, text='Un texto con una fuente y tamaño diferente!', font=('Courier', 12))
+lbl_con_fuente = tk.Label(ventana, text='Un texto con una fuente diferente!', font=('Arial'))
 lbl_con_fuente.pack()
+
+lbl_con_tamano = tk.Label(ventana, text='Un texto con un tamaño de fuente diferente!', font=('Courier', 15))
+lbl_con_tamano.pack()
+
+lbl_con_fuente_color_tamano = tk.Label(ventana, text='Soy un texto con fondo, fuente y tamaño personalizados!', background='blue', foreground='orange', font=('Times New Roman', 15))
+lbl_con_fuente_color_tamano.pack()
 
 ventana.mainloop()

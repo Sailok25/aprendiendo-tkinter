0660473fd963fe9170c2c31d24e02e1bd2b7cad8
Alba Ayala <albaayvi4@gmail.com>
2026-02-14 13:08:34 +0100

    Ej 2: Colores y Fuentes completado #2

--- 02_colores_fuentes.py
+++ 02_colores_fuentes.py
@@ -8,4 +8,7 @@ ventana.geometry("600x400")
 lbl_con_color_fondo = ttk.Label(ventana, text='Un texto con color de fondo!', background='green', foreground='white')
 lbl_con_color_fondo.pack()
 
+lbl_con_fuente = tk.Label(ventana, text='Un texto con una fuente y tamaño diferente!', font=('Courier', 12))
+lbl_con_fuente.pack()
+
 ventana.mainloop()
